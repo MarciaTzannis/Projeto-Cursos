5269821663d746dba8eaaff59f9e01d1c25164ae
MarciaTzannis <marciatzannis@gmail.com>
2019-04-04 22:23:49 -0300

    criado arquivo de validacao e resposta do formulario de compra

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     $cursos = [
-        "Full Stach" => ["Curso de desenvolvimento web", 1000.99, "full.jpeg", "fullstack"],
+        "Full Stack" => ["Curso de desenvolvimento web", 1000.99, "full.jpeg", "fullstack"],
         "Marketing Digital" => ["Curso de Marketing", 1000.98, "marketing.jpg", "marketing"],
         "UX" => ["Curso de User Experience", 9000.98, "ux.png", "ux"],
         "Mobile Android" => ["Curso de apps", 1000.97, "android.jpeg", "android"],
@@ -12,6 +12,7 @@
         "Senha" => "123456",
         "NivelAcesso" => mt_rand(0, 1)
     ];
+
 ?>
 
 <!DOCTYPE html>
@@ -81,7 +82,7 @@
                         <!-- descrição curso -->
                         <p><?php echo $infoCurso[0]; ?></p>
                         <!-- valor curso -->
-                        <p><?php echo $infoCursos[1]; ?></p>
+                        <p><?php echo $infoCurso[1]; ?></p>
                         <a href="#" class="btn btn-info" data-toggle="modal" data-target="<?php echo "#$infoCurso[3]" ?>" role="button">Comprar</a>
                     </div>
                 </div>
@@ -99,27 +100,32 @@
                             <h4 class="modal-title">Preencha os seus dados</h4>
                         </div>
                         <div class="modal-body">
-                            <form>
+                            <h4> Curso de: <?php echo $nomeCurso ?> </h4>
+                            <h4> Preço R$ <?php echo $infoCurso[1]; ?> </h4>
+                            <form action="validarCompra.php" method="post">
+                                <input type="hidden" name="nomeCurso" value="<?php echo $nomeCurso; ?>"> <!-- input hidden para esconder valores-->
+                                <input type="hidden" name="precoCurso" value="<?php echo $infoCurso[1]; ?>">
                                 <div class="input-group col-md-5">
                                     <label for="nomeCompleto">Nome Completo</label>
-                                    <input id="nomeCompleto" type="text" class="form-control">
+                                    <input id="nomeCompleto" name="nomeCompleto" type="text" class="form-control">
                                 </div>
                                 <div class="input-group col-md-5">
                                     <label for="cpf">CPF</label>
-                                    <input id="cpf" type="number" class="form-control">
+                                    <input id="cpf" name="cpf" type="number" class="form-control">
                                 </div>
                                 <div class="input-group col-md-5">
                                     <label for="numeroCartao">Número do Cartão</label>
-                                    <input id="numeroCartao" type="number" class="form-control">
+                                    <input id="numeroCartao" name="numeroCartao" type="number" class="form-control">
                                 </div>
                                 <div class="input-group col-md-5">
                                     <label for="validadeCartao">Validade do Cartão</label>
-                                    <input id="validadeCartao" type="month" class="form-control">
+                                    <input id="validadeCartao" name="validadeCartao" type="month" class="form-control">
                                 </div>
                                 <div class="input-group col-md-5">
-                                    <label for="cvv">Código de Segurança</label>
-                                    <input id="cvv" type="number" class="form-control">
+                                    <label for="cvv">CVV</label>
+                                    <input id="cvv" name="cvv" type="number" class="form-control">
                                 </div>
+                                <button class="btn btn-primary" type="submit">Comprar</button>
                             </form>
                         </div>
                         <div class="modal-footer">
